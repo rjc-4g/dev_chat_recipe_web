b48bcca38417711b4e125773ad17ff688c999159
sirensan <hosinahikaru@gmail.com>
2024-02-26 14:24:04 +0900

    選択リストの固定値取得API追加 ※リファクタリング前

--- backend/chatgptapi/views.py
+++ backend/chatgptapi/views.py
@@ -6,12 +6,13 @@ from .services.chatgpt import createPrompt
 
 import json
 
+
 class RequestChatGPTView(APIView):
     def post(self, request, format=None):
         try:
             # 質問結果取得
             content = createPrompt(request)
-            
+
             if not content:
                 return Response(
                     {"content": "結果が取得できませんでした。"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -27,28 +28,30 @@ class RequestChatGPTView(APIView):
         except Exception as e:
             # エラーが発生した場合
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
+
+
 class RequestChatGPTSelectionLang(APIView):
     def get(self, request, format=None):
         try:
             # 言語
             programming_language_open = open('./json/programmingLanguage.json', 'r')
-            programming_language_json = json.load(programming_language_open)
+            programming_language_json = json.load(programming_language_open)["lang"]
             return Response(
-                {programming_language_json}, status=status.HTTP_200_OK
+                str(programming_language_json), status=status.HTTP_200_OK
             )
         except Exception as e:
             # エラーが発生した場合
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class RequestChatGPTSelectionPlatform(APIView):
     def get(self, request, format=None):
         try:
             # プラットフォーム
             platform_open = open('./json/platform.json', 'r')
-            platform_json = json.load(platform_open)
+            platform_json = json.load(platform_open)["platform"]
             return Response(
-                {platform_json}, status=status.HTTP_200_OK
+                str(platform_json), status=status.HTTP_200_OK
             )
         except Exception as e:
             # エラーが発生した場合
